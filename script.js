1827d22a225d9a7724e79976746b682492d109cb
IloveMonday <iloveyouchandlerbing@gmail.com>
2023-12-20 01:46:44 -0500

    Create 5 game function

---
+++ script.js
@@ -0,0 +1,73 @@
+function computerSelect(){
+    const arr = ["rock", "paper", "scissors"];
+    let getComputerNumber = arr[Math.floor(Math.random() * arr.length)];
+    return getComputerNumber.toLowerCase();}
+
+
+
+function game() {
+  let playerScore = 0;
+  let computerScore = 0;
+
+
+
+  /*loop the game*/
+  for (i = 0; i < 5; i++) {
+    const computerSelection = computerSelect();
+    const playerSelection = prompt("Rock, Paper, Scissors?").toLowerCase();
+        /*PLAYER ALWAYS ROCK
+      const playerInput = "rock"; 
+      const playerSelection = playerInput.toLowerCase();*/
+    
+    console.log("Player: " + playerSelection);
+    console.log("Computer: " + computerSelection);
+    console.log(playRound(playerSelection, computerSelection));
+    console.log("Player Win Totals " + playerScore);
+    console.log("Computer Win Totals " + computerScore);
+  
+
+
+    function playRound(playerSelection, computerSelection) {
+      if (playerSelection === computerSelection) {
+        return "Draw!";
+      } else if (playerSelection === "rock") {
+        if (computerSelection === "scissors") {
+          playerScore++;
+          return "Player Wins";
+        } else {
+          computerScore++;
+          return "Computer Wins";
+        }
+      } else if (playerSelection === "scissors") {
+        if (computerSelection === "rock") {
+          playerScore++;
+          return "Player Wins";
+        } else {
+          computerScore++;
+          return "Computer Wins";
+        }
+      } else if (playerSelection === "paper") {
+        if (computerSelection === "rock") {
+          playerScore++;
+          return "Player Wins";
+        } else {
+          computerScore++;
+          return "Computer Wins";
+        }
+      }
+    }
+  }
+
+  if (playerScore > computerScore){
+    return "Player Wins It All!";
+  }
+  else if (playerScore < computerScore) { 
+    return "Computer Wins It All!";
+}
+    else {
+        return "It's a Draw!";
+    }
+
+}
+
+console.log(game());
